3361d274f1c01899184bf85a6b518a7e7aefdbf5
Rashidhafizuddin <93499349+Rashidhafizuddin@users.noreply.github.com>
2022-11-22 17:09:40 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+// onLoad
+//
+// $("document").ready(function()
+// {
+// $(".img1").animate({marginLeft:"500px"},3000)
+// $(".jt1").animate({marginRight:"400px"},3000)
+//
+//
+// });
+//
+// function checkPosition() {
+//     if (window.matchMedia('(max-width: 1000px)').matches) {
+//       $("document").ready(function()
+//       {
+//       $(".img1").animate({marginLeft:"0"},0)
+//       $(".jt1").animate({marginRight:"0"},0)
+//
+//     });
+//     } else {
+//       $("document").ready(function()
+//       {
+//       $(".img1").animate({marginLeft:"500px"},0)
+//       $(".jt1").animate({marginRight:"400px"},0)
+//
+//     }); .
+//     }
+// }
+
+// $(document).ready(function() {
+//     if($(window).width() >= 1024) {
+//       $(".img1").animate({marginLeft:"500px"},3000)
+//       $(".jt1").animate({marginRight:"400px"},3000)
+//     }
+//     else if($(window).width() <= 1024)
+//     {
+//       $(".img1").animate({marginLeft:"10px"},3000)
+//       $(".jt1").animate({marginRight:"10px"},3000)
+//     }
+// });
